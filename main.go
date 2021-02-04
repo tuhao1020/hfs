c287ca5649a4b160829f529facd2e8b2ec4c498d
tuhao <tuhao1020@qq.com>
2021-02-04 23:02:09 +0800

    add support for tls

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	dir := flag.String("dir", "./", "root directory")
 	port := flag.Int("port", 8090, "server port")
 	showHidden := flag.Bool("showHidden", false, "show hidden files or directories")
+	tls := flag.Bool("tls", false, "enable tls")
+	certFile := flag.String("certFile", "", "cert file")
+	keyFile := flag.String("keyFile", "", "key file")
 	flag.Parse()
 
 	if !core.IsExist(*dir) {
@@ -20,10 +23,22 @@ func main() {
 
 	app := iris.New()
 	app.Favicon("./favicon.ico")
+
+	// http file server
 	app.HandleDir("/", iris.Dir(*dir), iris.DirOptions{
 		ShowList:   true,
 		ShowHidden: *showHidden,
 		DirList:    core.TemplateDirList(),
 	})
-	_ = app.Listen(fmt.Sprintf(":%d", *port))
+
+	var err error
+	if *tls {
+		err = app.Run(iris.TLS(fmt.Sprintf(":%d", *port), *certFile, *keyFile))
+	} else {
+		err = app.Listen(fmt.Sprintf(":%d", *port))
+	}
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
